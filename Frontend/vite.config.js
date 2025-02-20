52ede9e41f0b4fd5497ad1c6861238e09ddaa66f
QaisarMoin <qaisarashraf685@gmail.com>
2025-02-20 17:20:28 +0530

    new vite.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,24 +3,24 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import fs from "fs";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    https:
-      command === "serve"
-        ? {
-            key: fs.readFileSync("localhost-key.pem"),
-            cert: fs.readFileSync("localhost-cert.pem"),
-          }
-        : false,
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      https:
+        command === "serve"
+          ? {
+              key: fs.readFileSync("localhost-key.pem"),
+              cert: fs.readFileSync("localhost-cert.pem"),
+            }
+          : false, // Disable HTTPS in production
+      headers: {
+        "Cross-Origin-Opener-Policy": "same-origin",
+        "Cross-Origin-Embedder-Policy": "require-corp",
+      },
+      proxy: {
+        "/api": "https://weblyss.onrender.com",
+      },
     },
-    proxy: {
-      // "/api": "http://localhost:3001",
-      "/api": "https://weblyss.onrender.com",
-    },
-  },
+  };
 });
