5d6ca2adb0eaeb68306a24f01368fe4904ab6d02
QaisarMoin <qaisarashraf685@gmail.com>
2025-02-20 17:12:47 +0530

    https setting

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    https: {
+      key: fs.readFileSync("localhost-key.pem"),
+      cert: fs.readFileSync("localhost-cert.pem"),
+    },
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
