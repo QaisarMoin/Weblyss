bb30621dcd333957f4fe693c4c43647007fcc9a2
QaisarMoin <qaisarashraf685@gmail.com>
2025-02-20 17:04:14 +0530

    Set COOP & COEP Headers in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,13 @@ import TestPrompt from "./src/TestPrompt.js";
 const app = express();
 
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
 app.use(express.json());
 dotenv.config();
 
