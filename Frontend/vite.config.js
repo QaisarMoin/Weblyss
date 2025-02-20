6552b108211f958650cc94e6c1a821d0fc64c0f1
QaisarMoin <qaisarashraf685@gmail.com>
2025-02-20 17:32:38 +0530

    new vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,29 +1,27 @@
+import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import fs from "fs";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { optimizeCssModules } from "vite-plugin-optimize-css-modules";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ mode }) => {
   return {
-    plugins: [react(), tailwindcss()],
+    build: {
+      target: "esnext",
+    },
     server: {
-      https:
-        command === "serve"
-          ? {
-              key: fs.readFileSync("localhost-key.pem"),
-              cert: fs.readFileSync("localhost-cert.pem"),
-            }
-          : false, // Disable HTTPS in production
       headers: {
         "Cross-Origin-Opener-Policy": "same-origin",
         "Cross-Origin-Embedder-Policy": "require-corp",
       },
-      proxy: {
-        "/api": "https://weblyss.onrender.com",
-      },
     },
-    build: {
-      target: "esnext", // Ensures compatibility with modern browsers
-    },
+    plugins: [
+      nodePolyfills({
+        include: ["path", "buffer"],
+      }),
+      UnoCSS(),
+      tsconfigPaths(),
+      mode === "production" && optimizeCssModules({ apply: "build" }),
+    ],
   };
 });
