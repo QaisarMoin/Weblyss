c496921b8c530fffc9311db73be58deb89e49532
QaisarMoin <qaisarashraf685@gmail.com>
2025-02-20 19:12:10 +0530

    vite.config reset

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,29 +1,18 @@
-import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { optimizeCssModules } from "vite-plugin-optimize-css-modules";
-import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  return {
-    build: {
-      target: "esnext",
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
-    server: {
-      headers: {
-        "Cross-Origin-Opener-Policy": "same-origin",
-        "Cross-Origin-Embedder-Policy": "require-corp",
-      },
+    proxy: {
+      // "/api": "http://localhost:3001",
+      "/api": "https://weblyss.onrender.com",
     },
-    plugins: [
-      react(),
-      nodePolyfills({
-        include: ["path", "buffer"],
-      }),
-      UnoCSS(),
-      tsconfigPaths(),
-      mode === "production" && optimizeCssModules({ apply: "build" }),
-    ],
-  };
+  },
 });
